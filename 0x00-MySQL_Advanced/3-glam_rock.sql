63bf5e6a8f111fac4653a8c74e313f952d77a5da
Willochs Ojigbo <82782869+Willochs316@users.noreply.github.com>
2023-04-07 01:36:04 +0100

    evaluated scripts that lists all bands with glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,11 @@
 -- script that lists all bands with Glam rock as their main style
 SELECT
-  name AS band_name,
-  EXTRACT(YEAR FROM NOW()) - CAST(SUBSTRING_INDEX(formed, '-', 1) AS UNSIGNED) AS lifespan
+  b.name AS band_name,
+  EXTRACT(YEAR FROM NOW()) - CAST(SUBSTRING_INDEX(b.formed, '-', 1) AS UNSIGNED) AS lifespan
 FROM
-  bands
+  bands b
+  JOIN styles s ON b.style_id = s.id
 WHERE
-  style LIKE '%Glam rock%'
+  s.name = 'Glam rock'
 ORDER BY
   lifespan DESC;
